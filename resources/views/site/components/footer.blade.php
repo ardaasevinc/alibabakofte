09ef26a1fb8a0d7e7653bfe001da774c326ef204
ardaasevinc <ardaasevinc@gmail.com>
2026-01-16 13:34:09 +0300

    footer center

--- resources/views/site/components/footer.blade.php
+++ resources/views/site/components/footer.blade.php
@@ -1,59 +1,61 @@
 @if($settings)
-<footer class="bg-primary pt120 pb120 pt-xs-40 pb-xs-64" style="margin-top:100px;">
-    <div class="container">
-        <div class="row">
-            
-            <div class="col-sm-8 mb-xs-24 text-center-xs">
-                {{-- Logo Alanı --}}
-                @if($settings->logo_light)
-                <div class="inline-block center">
-                    <img alt="{{ $settings->meta_title ?? 'Ali Baba Köfte' }}" 
-                         class="image-small display-block center mb-xs-24" 
-                         src="{{ asset('uploads/' . $settings->logo_light) }}" />
+    <footer class="bg-primary pt120 pb120 pt-xs-40 pb-xs-64" style="margin-top:100px;">
+        <div class="container">
+            <div class="row">
+                {{-- Tüm İçeriği Kapsayan ve Ortaya Hizalayan Sütun --}}
+                <div class="col-sm-12 text-center">
+
+                    {{-- Logo Alanı --}}
+                    @if($settings->logo_light)
+                        <div class="mb24">
+                            <img alt="{{ $settings->meta_title ?? 'Ali Baba Köfte' }}" class="image-small inline-block"
+                                src="{{ asset('uploads/' . $settings->logo_light) }}" />
+                        </div>
+                    @endif
+
+                    {{-- Metin Bilgileri --}}
+                    <div class="mb32">
+                        <h6 class="uppercase mb8">{{ $settings->meta_title ?? 'Ali Baba Köfte' }}</h6>
+
+                        @if($settings->slogan)
+                            <span class="sub display-block">{{ $settings->slogan }}</span>
+                        @endif
+
+                        @if($settings->address)
+                            <span class="sub display-block">{{ $settings->address }}</span>
+                        @endif
+
+                        @if($settings->work_time)
+                            <span class="sub display-block">
+                                Çalışma Saatleri: {!! strip_tags($settings->work_time) !!}
+                            </span>
+                        @endif
+                    </div>
+
+                    {{-- Sosyal Medya --}}
+                    <div>
+                        <h6 class="uppercase mb16">Bizi Takip Edin</h6>
+                        <ul class="list-inline social-list">
+                            @if($settings->facebook_url)
+                                <li>
+                                    <a href="{{ $settings->facebook_url }}" target="_blank">
+                                        <i class="ti-facebook"></i>
+                                    </a>
+                                </li>
+                            @endif
+
+                            @if($settings->instagram_url)
+                                <li>
+                                    <a href="{{ $settings->instagram_url }}" target="_blank">
+                                        <i class="ti-instagram"></i>
+                                    </a>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
+
                 </div>
-                @endif
-
-                <div class="inline-block display-block-xs text-center-xs">
-                    <h6 class="uppercase mb8">{{ $settings->meta_title ?? 'Ali Baba Köfte' }}</h6>
-                    
-                    @if($settings->slogan)
-                        <span class="sub display-block">{{ $settings->slogan }}</span>
-                    @endif
-
-                    @if($settings->address)
-                        <span class="sub display-block">{{ $settings->address }}</span>
-                    @endif
-
-                    @if($settings->work_time)
-                        {{-- Alt kısımda RichEditor verisini sade metin olarak göstermek isterseniz strip_tags kullanabilirsiniz --}}
-                        <span class="sub display-block">
-                            Çalışma Saatleri: {!! strip_tags($settings->work_time) !!}
-                        </span>
-                    @endif
                 </div>
-            </div>
-
-            <div class="col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 text-center">
-                <h6 class="uppercase">Bizi Takip Edin</h6>
-                <ul class="list-inline social-list">
-                    @if($settings->facebook_url)
-                    <li>
-                        <a href="{{ $settings->facebook_url }}" target="_blank">
-                            <i class="ti-facebook"></i>
-                        </a>
-                    </li>
-                    @endif
-
-                    @if($settings->instagram_url)
-                    <li>
-                        <a href="{{ $settings->instagram_url }}" target="_blank">
-                            <i class="ti-instagram"></i>
-                        </a>
-                    </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-        </div>
-    </footer>
+                </div>
+                </footer>
 @endif
