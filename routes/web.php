64522b17861435bcfe6c6b3ad5058ebb35626d4e
ardaasevinc <ardaasevinc@gmail.com>
2026-01-16 10:33:28 +0300

    test4

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ Route::get('/menu', [MenuIndexController::class, 'index'])->name('site.menu.inde
 
 
 
-Route::get('/instagram/callback', function (Illuminate\Http\Request $request) {
-    // Meta'nın gönderdiği token ile sizin config'deki token eşleşmeli
-    if ($request->input('hub_verify_token') === config('services.instagram.verify_token')) {
-        // Hem hub_challenge hem de hub.challenge (hub_challenge olarak gelir) kontrolü
+Route::get('/instagram/callback', function (Request $request) {
+    // Eğer Meta bir challenge kodu gönderdiyse, onu direkt ekrana yaz
+    if ($request->has('hub_challenge')) {
         return response($request->input('hub_challenge'), 200)
-                 ->header('Content-Type', 'text/plain');
+            ->header('Content-Type', 'text/plain');
     }
 
-    return response('Token mismatch veya istek hatalı', 403);
+    
+    return "Meta'dan istek bekleniyor...";
 });
