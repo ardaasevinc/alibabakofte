0fb8c46294547091fe79fa1e8c9df86054856c7c
ardaasevinc <ardaasevinc@gmail.com>
2026-01-16 13:08:26 +0300

    footer data

--- resources/views/site/components/footer.blade.php
+++ resources/views/site/components/footer.blade.php
@@ -1,40 +1,59 @@
+@if($settings)
 <footer class="bg-primary pt120 pb120 pt-xs-40 pb-xs-64" style="margin-top:100px;">
     <div class="container">
         <div class="row">
             
-            
             <div class="col-sm-6 mb-xs-24 text-center-xs">
+                {{-- Logo Alanı --}}
+                @if($settings->logo_light)
                 <div class="inline-block center">
-                    <img alt="Logo" class="image-small display-block center mb-xs-24" src="{{ asset('site/alibaba/logos/logo-white.svg') }}" />
+                    <img alt="{{ $settings->meta_title ?? 'Ali Baba Köfte' }}" 
+                         class="image-small display-block center mb-xs-24" 
+                         src="{{ asset('uploads/' . $settings->logo_light) }}" />
                 </div>
+                @endif
+
                 <div class="inline-block display-block-xs text-center-xs">
-                    <h6 class="uppercase mb8">Ali Baba Köfte</h6>
-                    <span class="sub display-block">29 yıldır köftenin Ali Babası</span>
-                    <span class="sub display-block">Ferhatpaşa mah. Ulus cad. No:14</span>
-                    <span class="sub display-block">Çalışma Saatleri 05:30 - 20:00</span>
+                    <h6 class="uppercase mb8">{{ $settings->meta_title ?? 'Ali Baba Köfte' }}</h6>
+                    
+                    @if($settings->slogan)
+                        <span class="sub display-block">{{ $settings->slogan }}</span>
+                    @endif
+
+                    @if($settings->address)
+                        <span class="sub display-block">{{ $settings->address }}</span>
+                    @endif
+
+                    @if($settings->work_time)
+                        {{-- Alt kısımda RichEditor verisini sade metin olarak göstermek isterseniz strip_tags kullanabilirsiniz --}}
+                        <span class="sub display-block">
+                            Çalışma Saatleri: {!! strip_tags($settings->work_time) !!}
+                        </span>
+                    @endif
                 </div>
             </div>
-            
-
 
             <div class="col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 text-center">
                 <h6 class="uppercase">Bizi Takip Edin</h6>
                 <ul class="list-inline social-list">
-                    
+                    @if($settings->facebook_url)
                     <li>
-                        <a href="#">
+                        <a href="{{ $settings->facebook_url }}" target="_blank">
                             <i class="ti-facebook"></i>
                         </a>
                     </li>
+                    @endif
+
+                    @if($settings->instagram_url)
                     <li>
-                        <a href="#">
+                        <a href="{{ $settings->instagram_url }}" target="_blank">
                             <i class="ti-instagram"></i>
                         </a>
                     </li>
+                    @endif
                 </ul>
             </div>
         </div>
-        <!--end of row-->
-    </div>
-    <!--end of container-->
-</footer>
+        </div>
+    </footer>
+@endif
