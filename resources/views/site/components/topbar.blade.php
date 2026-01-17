0b39da65d3b0e584603168462d3f9e1148e5a5e2
ardaasevinc <ardaasevinc@gmail.com>
2026-01-17 16:33:42 +0300

    test

--- resources/views/site/components/topbar.blade.php
+++ resources/views/site/components/topbar.blade.php
@@ -1,31 +1,49 @@
 <div class="container">
-                        <div class="row">
-                            <div class="col-sm-4  text-center-xs">
-                                <span class="sub">Çatalca, İstanbul</span>
-                            </div>
-                            <div class="col-sm-4 text-center">
-                                <ul class="list-inline social-list">
-                                    <li>
-                                        <a href="{{ url('https://www.google.com/search?hl=en-TR&gl=tr&q=%C3%87atalca%E2%80%99l%C4%B1+Ali+Baba+K%C3%B6fte+Salonu,+Ferhatpa%C5%9Fa+mah.+Ulus+cad.+No:14+%C3%87atalca+adliyesine,+D:200m,+34540+%C3%87atalca/%C4%B0stanbul&ludocid=16427161983458439926&lsig=AB86z5WyONQeNVlausRDLwV98KGr#lrd=0x14b54519a0a305bb:0xe3f900ca03d772f6,3') }}" style="color:white;" target="_blank">
-                                            <span class="sub">Google'da Yorum yap</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="ti-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="ti-instagram"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="col-sm-4 text-right text-center-xs">
-                                <span class="sub">Çalışma Saatleri: 5:30 - 20:00</span>
-                            </div>
-                        </div>
-                        <!--end of row-->
-                    </div>
-                    <!--end of container-->
+    <div class="row">
+        {{-- Sol Taraf: Adres (Sadece Şehir/Semt Kısmı veya Kısa Adres) --}}
+        <div class="col-sm-4 text-center-xs">
+            <span class="sub">
+                {{ $settings?->address ? Str::limit($settings->address, 30) : 'Çatalca, İstanbul' }}
+            </span>
+        </div>
+
+        {{-- Orta Taraf: Sosyal Medya ve Google Yorum --}}
+        <div class="col-sm-4 text-center">
+            <ul class="list-inline social-list">
+                {{-- Google Yorum Yap Linki --}}
+                @if($settings?->gpage_comment)
+                    <li>
+                        <a href="{{ $settings->gpage_comment }}" style="color:white;" target="_blank">
+                            <span class="sub">Google'da Yorum yap</span>
+                        </a>
+                    </li>
+                @endif
+
+                {{-- Facebook --}}
+                @if($settings?->facebook_url)
+                    <li>
+                        <a href="{{ $settings->facebook_url }}" target="_blank">
+                            <i class="ti-facebook"></i>
+                        </a>
+                    </li>
+                @endif
+
+                {{-- Instagram --}}
+                @if($settings?->instagram_url)
+                    <li>
+                        <a href="{{ $settings->instagram_url }}" target="_blank">
+                            <i class="ti-instagram"></i>
+                        </a>
+                    </li>
+                @endif
+            </ul>
+        </div>
+
+        {{-- Sağ Taraf: Çalışma Saatleri --}}
+        <div class="col-sm-4 text-right text-center-xs">
+            <span class="sub">
+                Çalışma Saatleri: {!! $settings?->work_time ? strip_tags($settings->work_time) : '05:30 - 20:00' !!}
+            </span>
+        </div>
+    </div>
+    </div>
