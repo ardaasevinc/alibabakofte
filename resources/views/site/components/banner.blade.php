378e82dce32d2a3e9489891205ee19968503b350
ardaasevinc <ardaasevinc@gmail.com>
2026-01-17 16:53:34 +0300

    banner image

--- resources/views/site/components/banner.blade.php
+++ resources/views/site/components/banner.blade.php
@@ -1,16 +1,18 @@
-@if($banner && $banner->count() > 0)
+@if(isset($banner) && (is_array($banner) || $banner instanceof \Illuminate\Support\Collection) && count($banner) > 0)
     <section class="pb0 pt0">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 text-center">
-                    {{-- data-animation="fade" yumuşak geçişi, data-speed ise süreyi belirler --}}
+                    {{-- data-animation="fade" yumuşak geçişi sağlar --}}
                     <div class="image-slider slider-arrow-controls mb0" data-animation="fade" data-speed="5000">
                         <ul class="slides">
                             @foreach($banner as $item)
-                                <li class="v-align-transform">
-                                    <img alt="{{ $item->title ?? 'Ali Baba Köfte' }}"
-                                        src="{{ asset('uploads/' . $item?->image) }}" />
-                                </li>
+                                @if(!empty($item->image))
+                                    <li class="v-align-transform">
+                                        <img alt="{{ $item->title ?? 'Ali Baba Köfte' }}" 
+                                             src="{{ asset('uploads/' . $item->image) }}" />
+                                    </li>
+                                @endif
                             @endforeach
                         </ul>
                     </div>
@@ -18,18 +20,19 @@
             </div>
         </div>
     </section>
+@else
+    {{-- Banner yoksa veya hata varsa boş kalmasın diye istersen buraya sabit bir görsel koyabilirsin --}}
 @endif
 
-{{-- Temadaki slider'ın fade (solma) modunda çalışması için manuel tetikleme --}}
 <script>
     document.addEventListener("DOMContentLoaded", function() {
         if (typeof $.flexslider === "function") {
             $('.image-slider').flexslider({
-                animation: "fade",      // Kayma yerine solma efekti
-                slideshowSpeed: 5000,   // Görselin ekranda kalma süresi (5 sn)
-                animationSpeed: 1000,   // Geçiş sırasındaki solma hızı (1 sn)
-                controlNav: false,      // Alt noktaları gizle
-                directionNav: true      // Sağ-sol oklarını göster
+                animation: "fade",      
+                slideshowSpeed: 5000,   
+                animationSpeed: 1000,   
+                controlNav: false,      
+                directionNav: true      
             });
         }
     });
