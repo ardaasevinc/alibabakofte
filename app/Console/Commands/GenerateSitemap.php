843114e9933dc052d874d51d952ed93b3862999c
ardaasevinc <ardaasevinc@gmail.com>
2026-01-17 17:28:05 +0300

    sitemap blogs

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -12,12 +12,25 @@ class GenerateSitemap extends Command
 
     protected $description = 'Sitemap dosyasını otomatik oluşturur';
 
-    public function handle()
-    {
-        // APP_URL üzerinden taramaya başlar ve public/sitemap.xml olarak kaydeder
-        SitemapGenerator::create(config('app.url'))
-            ->writeToFile(public_path('sitemap.xml'));
+    // app/Console/Commands/GenerateSitemap.php
 
-        $this->info('Sitemap başarıyla oluşturuldu!');
-    }
+public function handle()
+{
+    $sitemap = \Spatie\Sitemap\Sitemap::create();
+
+    // Ana Sayfalar
+    $sitemap->add(\Spatie\Sitemap\Tags\Url::create('/')->setPriority(1.0))
+            ->add(\Spatie\Sitemap\Tags\Url::create('/blog')->setPriority(0.8))
+            ->add(\Spatie\Sitemap\Tags\Url::create('/menu')->setPriority(0.8));
+
+    // Dinamik Blog Yazılarını Ekle (Başlık değil, URL olarak)
+    \App\Models\Blog::all()->each(function (\App\Models\Blog $blog) use ($sitemap) {
+        $sitemap->add(\Spatie\Sitemap\Tags\Url::create("/blog/{$blog->slug}")
+            ->setLastModificationDate($blog->updated_at)
+            ->setChangeFrequency(\Spatie\Sitemap\Tags\Url::CHANGE_FREQUENCY_WEEKLY)
+            ->setPriority(0.6));
+    });
+
+    $sitemap->writeToFile(public_path('sitemap.xml'));
+}
 }
