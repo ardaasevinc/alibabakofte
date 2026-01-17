a130375477e7d6c645653fa2f095d041a1671785
ardaasevinc <ardaasevinc@gmail.com>
2026-01-17 15:07:31 +0300

    spesyal model

--- resources/views/site/components/special.blade.php
+++ resources/views/site/components/special.blade.php
@@ -1,34 +1,45 @@
-<a id="special"></a>
-<section class="pb0">
-    <div class="container">
-        <div class="row  mb64 mb-xs-40">
-            <div class="col-sm-12 text-center">
-                <div class="ribbon">
-                    <h6 class="uppercase mb0">Sezon Spesiyal</h6>
+@if($specials && $specials->count() > 0)
+    <a id="special"></a>
+    @foreach($specials as $special)
+        <section class="pb0">
+            <div class="container">
+                <div class="row mb64 mb-xs-40">
+                    <div class="col-sm-12 text-center">
+                        <div class="ribbon">
+                            {{-- Başlık varsa göster, yoksa varsayılan metni bas --}}
+                            <h6 class="uppercase mb0">{{ $special->title ?? 'Sezon Spesiyali' }}</h6>
+                        </div>
+                    </div>
+                </div>
+                @if($special->image)
+                    <div class="row mb32 mb-xs-24">
+                        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                            <img alt="{{ $special->title }}" src="{{ asset('uploads/' . $special->image) }}" />
+                        </div>
+                    </div>
+                @endif
+                <div class="row">
+                    <div class="col-md-7 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                        @if($special->title)
+                            <h2 class="alt-font">
+                                {!! nl2br(e($special->title)) !!}
+                            </h2>
+                        @endif
+
+                        @if($special->desc)
+                            <div class="mb0">
+                                {!! $special->desc !!}
+                            </div>
+                        @endif
+
+                        @if($special->price)
+                            <span class="block bold mt16" style="font-size: 20px;">
+                                {{ number_format($special->price, 0, ',', '.') }}₺
+                            </span>
+                        @endif
+                    </div>
                 </div>
             </div>
-        </div>
-        <!--end of row-->
-        <div class="row mb32 mb-xs-24">
-            <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
-                <img alt="Special" src="site/img/special.jpg" />
-            </div>
-        </div>
-        <!--end of row-->
-        <div class="row">
-            <div class="col-md-7 col-md-offset-3 col-sm-8 col-sm-offset-2">
-                <h2 class="alt-font">Spanner Crab
-                    <br /> salad w/ raw shaved
-                    <br />vegetables
-                </h2>
-                <p class="mb0">
-                    w/ kale, broccoli flowerette, spiced pumpkin, barley,
-                    <br class="hidden-sm hidden-xs" /> green chilli & pepitas on house made life bread with light
-                    <br class="hidden-sm hidden-xs" /> cottage cheese & free range eggs.
-                </p>
-                <span class="block">18</span>
-            </div>
-        </div>
-    </div>
-    <!--end of container-->
-</section>
+            </section>
+    @endforeach
+@endif
