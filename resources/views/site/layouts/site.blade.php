25132c63e90a40a0150b30dfab4156cdebd8ec9c
ardaasevinc <ardaasevinc@gmail.com>
2026-01-17 16:16:45 +0300

    head from db

--- resources/views/site/layouts/site.blade.php
+++ resources/views/site/layouts/site.blade.php
@@ -3,34 +3,56 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Meşhur Çatalcalı Ali Baba Köfte Salonu | 1997'den Beri Gelen Lezzet</title>
-    <meta name="viewport" content="width=device-width, initial-scale=0.9">
+    {{-- SEO Başlık --}}
+    <title>{{ $settings->meta_title ?? 'Meşhur Çatalcalı Ali Baba Köfte Salonu | 1997\'den Beri Gelen Lezzet' }}</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    {{-- SEO Meta Etiketleri --}}
+    <meta name="description" content="{{ $settings->meta_desc ?? 'Çatalca\'nın meşhur tarihi köftecisi Ali Baba Köfte Salonu. 1997\'den beri Çatalca merkezde değişmeyen reçetesiyle hizmet veren Ali Baba.' }}">
+    <meta name="keywords" content="{{ $settings->meta_keywords ?? 'çatalca köfte, meşhur çatalca köftecisi, ali baba köfte çatalca, alibaba köfte' }}">
+    <meta name="author" content="Ali Baba Köfte Salonu">
+    <meta name="robots" content="index, follow">
+
+    {{-- Open Graph (Sosyal Medya Paylaşım) --}}
+    <meta property="og:title" content="{{ $settings->meta_title }}">
+    <meta property="og:description" content="{{ $settings->meta_desc }}">
+    <meta property="og:image" content="{{ $settings->logo_dark ? asset('uploads/' . $settings->logo_dark) : asset('site/alibaba/logos/logo-white.svg') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
+
+    {{-- CSS Dosyaları --}}
     <link href="{{ asset('site/css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/themify-icons.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/flexslider.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/theme.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/custom.css') }}" rel="stylesheet" type="text/css" media="all" />
-    <link rel="icon" type="image/svg+xml" href="{{ asset('site/alibaba/logos/favicon.svg') }}">
-    <link rel="alternate icon" type="image/png" href="{{ asset('site/alibaba/logos/favicon.png') }}">
-    <link rel="mask-icon" href="{{ asset('site/alibaba/logos/favicon.svg') }}" color="#000000">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('site/alibaba/logos/favicon.svg') }}">
 
+    {{-- Favicon - Dinamik --}}
+    @if($settings->favicon)
+        <link rel="icon" type="image/png" href="{{ asset('uploads/' . $settings->favicon) }}">
+        <link rel="apple-touch-icon" href="{{ asset('uploads/' . $settings->favicon) }}">
+    @else
+        <link rel="icon" type="image/svg+xml" href="{{ asset('site/alibaba/logos/favicon.svg') }}">
+        <link rel="alternate icon" type="image/png" href="{{ asset('site/alibaba/logos/favicon.png') }}">
+    @endif
 
-    <link href='https://fonts.googleapis.com/css?family=Montserrat%7CInconsolata:400,700%7CPathway+Gothic+One'
-        rel='stylesheet' type='text/css'>
-    <meta name="description"
-        content="Çatalca'nın meşhur tarihi köftecisi Ali Baba Köfte Salonu. 1997'den beri Çatalca merkezde değişmeyen reçetesiyle hizmet veren Ali Baba'nın eşsiz köftelerini deneyin.">
-    <meta name="keywords"
-        content="çatalca köfte, meşhur çatalca köftecisi, ali baba köfte çatalca, tarihi çatalca köftesi, çatalca merkez restoran, çatalca yemek yerleri, alibaba köfte, çatalca köfte salonu, ali baba köfte salonu">
-    <meta name="author" content="Ali Baba Köfte Salonu">
+    {{-- Fontlar --}}
+    <link href='https://fonts.googleapis.com/css?family=Montserrat%7CInconsolata:400,700%7CPathway+Gothic+One' rel='stylesheet' type='text/css'>
 
-    <meta property="og:title" content="Meşhur Çatalcalı Ali Baba Köfte Salonu">
-    <meta property="og:description" content="1997'den beri Çatalca'da köftenin Ali Babası. Gerçek lezzet, tarihi doku.">
-    <meta property="og:image" content="{{ asset('site/alibaba/logos/logo-white.svg') }}">
-    <meta property="og:url" content="https://alibabakofte.com.tr">
-    <meta property="og:type" content="website">
-    <meta name="robots" content="index, follow">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- Google Analytics Code --}}
+    @if($settings->google_analytics_code)
+        {!! $settings->google_analytics_code !!}
+    @endif
+
+    {{-- Facebook Pixel Code --}}
+    @if($settings->facebook_pixel_code)
+        {!! $settings->facebook_pixel_code !!}
+    @endif
+
+    {{-- UTM Takibi İçerik Kontrolü --}}
+    @if(request()->has('utm_content'))
+        @endif
 </head>
 
 <body>
