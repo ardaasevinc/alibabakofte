dc5a3b37934f30b156ce2bae53ed7307b21339c2
ardaasevinc <ardaasevinc@gmail.com>
2026-01-19 12:23:41 +0300

    ali baba köfte

--- resources/views/site/layouts/site.blade.php
+++ resources/views/site/layouts/site.blade.php
@@ -3,36 +3,38 @@
 
 <head>
     <meta charset="utf-8">
-    {{-- SEO Başlık --}}
-    <title>{{ $settings?->meta_title ?? 'Meşhur Çatalcalı Ali Baba Köfte Salonu | 1997\'den Beri Gelen Lezzet' }}
+    <title>
+        @if(isset($blog))
+            {{ $blog->title }} | {{ $settings?->title_suffix ?? 'Meşhur Çatalcalı Ali Baba Köfte' }}
+        @else
+            {{ $settings?->meta_title ?? 'Meşhur Çatalcalı Ali Baba Köfte Salonu | 1997\'den Beri Gelen Lezzet' }}
+        @endif
     </title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    {{-- SEO Meta Etiketleri --}}
     <meta name="description"
-        content="{{ $settings?->meta_desc ?? 'Çatalca\'nın meşhur tarihi köftecisi Ali Baba Köfte Salonu. 1997\'den beri Çatalca merkezde değişmeyen reçetesiyle hizmet veren Ali Baba.' }}">
+        content="{{ isset($blog) ? Str::limit(strip_tags($blog->desc), 160) : ($settings?->meta_desc ?? 'Çatalca\'nın meşhur tarihi köftecisi Ali Baba Köfte Salonu.') }}">
+
     <meta name="keywords"
-        content="{{ $settings->meta_keywords ?? 'çatalca köfte, meşhur çatalca köftecisi, ali baba köfte çatalca, alibaba köfte' }}">
+        content="{{ (isset($blog) && is_array($blog->tags)) ? implode(', ', $blog->tags) : ($settings->meta_keywords ?? 'çatalca köfte, meşhur çatalca köftecisi, ali baba köfte çatalca') }}">
+    
     <meta name="author" content="selquor.com">
     <meta name="robots" content="index, follow">
 
-    {{-- Open Graph (Sosyal Medya Paylaşım) --}}
-    <meta property="og:title" content="{{ $settings?->meta_title }}">
-    <meta property="og:description" content="{{ $settings?->meta_desc }}">
+    <meta property="og:title" content="{{ isset($blog) ? $blog->title : $settings?->meta_title }}">
+    <meta property="og:description" content="{{ isset($blog) ? Str::limit(strip_tags($blog->desc), 160) : $settings?->meta_desc }}">
     <meta property="og:image"
-        content="{{ $settings?->logo_dark ? asset('uploads/' . $settings?->logo_dark) : asset('site/alibaba/logos/logo-white.svg') }}">
+        content="{{ (isset($blog) && $blog->image) ? asset('uploads/' . $blog->image) : ($settings?->logo_dark ? asset('uploads/' . $settings?->logo_dark) : asset('site/alibaba/logos/logo-white.svg')) }}">
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:type" content="website">
+    <meta property="og:type" content="{{ isset($blog) ? 'article' : 'website' }}">
 
-    {{-- CSS Dosyaları --}}
     <link href="{{ asset('site/css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/themify-icons.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/flexslider.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/theme.css') }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset('site/css/custom.css') }}" rel="stylesheet" type="text/css" media="all" />
 
-    {{-- Favicon - Dinamik --}}
     @if($settings?->favicon)
         <link rel="icon" type="image/png" href="{{ asset('uploads/' . $settings?->favicon) }}">
         <link rel="apple-touch-icon" href="{{ asset('uploads/' . $settings?->favicon) }}">
@@ -41,7 +43,6 @@
         <link rel="alternate icon" type="image/png" href="{{ asset('site/alibaba/logos/favicon.png') }}">
     @endif
 
-    {{-- Fontlar --}}
     <link href='https://fonts.googleapis.com/css?family=Montserrat%7CInconsolata:400,700%7CPathway+Gothic+One'
         rel='stylesheet' type='text/css'>
 
@@ -56,15 +57,13 @@
     <script>
         !function (f, b, e, v, n, t, s) {
             if (f.fbq) return; n = f.fbq = function () {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
             };
             if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
             n.queue = []; t = b.createElement(e); t.async = !0;
             t.src = v; s = b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+        }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
         fbq('init', '{{ $settings->facebook_pixel_code }}');
         fbq('track', 'PageView');
@@ -76,12 +75,6 @@
         gtag('js', new Date());
         gtag('config', '{{ $settings->google_analytics_code }}');
     </script>
-
-    {{-- UTM Takibi İçerik Kontrolü --}}
-    @if(request()->has('utm_content'))
-    @endif
-
-
 </head>
 
 <body>
